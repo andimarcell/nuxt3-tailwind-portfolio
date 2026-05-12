c778eff85e786d0087459c37318db042d0df8f13
Andi <andimarsituru0064@gmail.com>
2026-05-12 18:27:12 +0800

    01.Cara Menambahkan Parameter Route Di Page
    - Menambahkan tag nav global di dalam app.vue menggunakan komponen NuxtLink untuk navigasi SPA.
    
    - Menyematkan komponen NuxtPage sebagai portal sistem render halaman dinamis.
    
    - Membuat pages/index.vue sebagai halaman utama dan setup reaktivitas URL.

--- app/app.vue
+++ app/app.vue
@@ -1,6 +1,12 @@
 <template>
   <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <nav>
+      <ul>
+        <li><NuxtLink to="/">Home</NuxtLink></li>
+        <li><NuxtLink to="/about">About</NuxtLink></li>
+        <li><NuxtLink to="/blog">Blog</NuxtLink></li>
+      </ul>
+    </nav>
   </div>
+  <NuxtPage />
 </template>
