6912dab2b13ee9c5e87de28569bea4b85348af15
Andi <andimarsituru0064@gmail.com>
2026-05-12 18:28:40 +0800

    01.Cara Berpindah Halaman Di Nuxt.js
    - Menambahkan tag nav global di dalam app.vue menggunakan komponen NuxtLink untuk navigasi SPA.
    
    - Menyematkan komponen NuxtPage sebagai portal sistem render halaman dinamis.
    
    - Membuat pages/index.vue sebagai halaman utama dan setup reaktivitas URL.

---
+++ app/pages/index.vue
@@ -0,0 +1,13 @@
+<script setup>
+
+const route = useRoute()
+
+</script>
+
+<template>
+    <div>
+        <h1>Home</h1>
+        <p>Curren route: {{ route.path }}</p>
+        <a href="https://nuxt.com/docs/getting-started/routing" target="_blank">Learn more about routing</a>
+    </div>
+</template>
