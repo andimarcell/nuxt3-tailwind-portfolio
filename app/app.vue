c1293dbca4900d6c0dc0ab565534e6bc04477ed9
Andi <andimarsituru0064@gmail.com>
2026-05-12 20:39:20 +0800

    03.Menentukan Layout Halaman Di Nuxt
    - Menambahkan custom layout bernama app.vue di dalam direktori layouts untuk membungkus navigasi global.
    
    - Menggunakan properti name pada komponen NuxtLayout di file root untuk memanggil layout secara spesifik.
    
    - Mengintegrasikan komponen NuxtPage dan slot agar konten halaman dari direktori pages dapat dirender secara dinamis.

--- app/app.vue
+++ app/app.vue
@@ -1,12 +1,5 @@
 <template>
-  <div>
-    <nav>
-      <ul>
-        <li><NuxtLink to="/">Home</NuxtLink></li>
-        <li><NuxtLink to="/about">About</NuxtLink></li>
-        <li><NuxtLink to="/blog">Blog</NuxtLink></li>
-      </ul>
-    </nav>
-  </div>
-  <NuxtPage />
+  <NuxtLayout name="app">
+    <NuxtPage />
+  </NuxtLayout>
 </template>
