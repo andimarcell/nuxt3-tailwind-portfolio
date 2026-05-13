cedf75770e849e500c3fc7a0a2b1170a87b342b0
Andi <andimarsituru0064@gmail.com>
2026-05-13 20:24:18 +0800

    05.Nuxt Modules Dan Cara Install Tailwindcss Di Nuxt
    - Install tailwindcss and @tailwindcss/vite packages
    
    - Inject Vite plugin and register global main.css in nuxt.config.ts
    
    - Initialize @import 'tailwindcss' in main.css
    
    - Setup boilerplate index.vue with app layout binding

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -8,7 +8,7 @@ const route = useRoute()
 
 <template>
     <div>
-        <h1>Home</h1>
+        <h1 class="">Home</h1>
         <p>Curren route: {{ route.path }}</p>
         <a href="https://nuxt.com/docs/getting-started/routing" target="_blank">Learn more about routing</a>
     </div>
