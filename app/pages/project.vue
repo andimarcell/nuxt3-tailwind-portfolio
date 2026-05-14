c56cbd2f17fde2a7af76c952bc4aaeb03bc644d0
Andi <andimarsituru0064@gmail.com>
2026-05-14 18:05:21 +0800

    11.Mengenal Data Fetching Dengan Usefetch
    migrasi pengambilan data ke useFetch dan tambah status antarmuka
    
    Ganti penggunaan fetch biasa menjadi composable useFetch bawaan Nuxt untuk mencegah penarikan data ganda
    
    Terapkan proses destructuring untuk mengambil status pending error dan data dari respons API GitHub
    
    Tambahkan rendering kondisional pada template untuk menampilkan indikator pemuatan pesan galat dan hasil penarikan data

--- app/pages/project.vue
+++ app/pages/project.vue
@@ -8,8 +8,15 @@ useHead({
 });
 
 const project = $fetch('https://api.github.com/users/andimarcell/repos'); console.log(project);
+
+// const projects = useFetch('https://api.github.com/users/andimarcell/repos'); console.log(projects);
+
+const {error, pending, data} = await useFetch('https://api.github.com/users/andimarcell/repos');
 </script>
 
 <template>
     <h1>Project</h1>
+    <section v-if="pending">Loading</section>
+    <section v-else-if="error">Something went wrong</section>
+    <section v-else>{{ data }}</section>
 </template>
