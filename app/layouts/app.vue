f012189e67fb654daa7fc5ca0dac6d8211d35747
Andi <andimarsituru0064@gmail.com>
2026-05-14 16:28:12 +0800

    09.Cara Konfigurasi Seo Page Di Nuxt
    Tambahkan atribut lang id pada tag html untuk SEO lokal
    
    Konfigurasi favicon untuk identitas tab browser
    
    Lengkapi Open Graph URL dan Twitter image untuk kebutuhan preview sosmed
    
    Sesuaikan metadata dengan identitas portofolio Andi Marcell

--- app/layouts/app.vue
+++ app/layouts/app.vue
@@ -1,7 +1,11 @@
 <script setup>
 useHead({
+  htmlAttrs: {
+    lang: 'id' 
+  },
   titleTemplate: '%s - Porto',
   link: [
+    // Preconnect untuk Google Fonts
     {
       rel: 'preconnect',
       href: 'https://fonts.googleapis.com',
@@ -10,9 +14,24 @@ useHead({
       rel: 'preconnect',
       href: 'https://fonts.googleapis.com/css2?family=Roboto&display=swap',
       crossorigin: 'anonymous',
-    }
+    },
+    // Favicon (Nanti siapkan gambar favicon.ico di folder public/)
+    { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
   ]
 });
+
+useSeoMeta({
+   title: 'Porto - Andi Marcell',
+  description: 'Portofolio dan blog pribadi Andi Marcell, seorang Junior Frontend Engineer dari Balikpapan.',
+  ogTitle: 'Porto - Andi Marcell',
+  ogDescription: 'Portofolio dan blog pribadi Andi Marcell, seorang Junior Frontend Engineer dari Balikpapan.',
+  ogImage: 'https://domainku.com/preview.png', // Ganti dengan link gambar asli nanti saat deploy
+  ogUrl: 'https://domainku.com', // Ganti dengan link web asli nanti
+  twitterCard: 'summary_large_image',
+  twitterTitle: 'Porto - Andi Marcell',
+  twitterDescription: 'Portofolio dan blog pribadi Andi Marcell, seorang Junior Frontend Engineer dari Balikpapan.',
+  twitterImage: 'https://domainku.com/preview.png' // Samakan dengan ogImage
+})
 </script>
 
 <template>
