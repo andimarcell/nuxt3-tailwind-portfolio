e786e7da4d88394b53720ac7cb2ad7016bc6f20d
Andi <andimarsituru0064@gmail.com>
2026-05-17 22:56:10 +0800

    33.Menambahkan Efek Transisi Ketika Berpindah Halaman
    Feat(app): implementasi transisi halaman global dengan pageTransition
    
    Mengonfigurasi app.pageTransition pada nuxt.config.ts dengan mode out-in untuk memastikan transisi yang halus antar rute.
    
    Menambahkan properti devtools timeline pada konfigurasi guna memantau performa transisi dan aktivitas aplikasi secara real-time.
    
    Memperbarui app.vue dengan mendefinisikan aturan CSS transisi untuk efek visual grayscale dan opasitas saat navigasi halaman berlangsung.
    
    Memastikan struktur layout konsisten dengan menyertakan NuxtPage di dalam NuxtLayout untuk mendukung transisi halaman yang responsif.

--- app/app.vue
+++ app/app.vue
@@ -3,3 +3,20 @@
     <NuxtPage />
   </NuxtLayout>
 </template>
+
+<style>
+.page-enter-active,
+.page-leave-active {
+  transition: all 0.3s ease-out;
+}
+
+.page-enter-from {
+  opacity: 0;
+  filter: grayscale(1);
+}
+
+.page-leave-to {
+  opacity: 0;
+  filter: grayscale(1);
+}
+</style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,31 +1,32 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
-import tailwindcss from "@tailwindcss/vite"
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
-  devtools: { enabled: true },
-  css: ['~/assets/css/main.css'],
+  compatibilityDate: "2025-07-15",
+  devtools: { enabled: true, timeline: { enabled: true } },
+  app: {
+    pageTransition: { name: "page", mode: "out-in" },
+  },
+  css: ["~/assets/css/main.css"],
   vite: {
-    plugins: [
-      tailwindcss(),
-    ],
+    plugins: [tailwindcss()],
   },
-  
-  modules: ['@nuxtjs/color-mode','@nuxt/content'],
+
+  modules: ["@nuxtjs/color-mode", "@nuxt/content"],
   colorMode: {
-    classSuffix: '',
+    classSuffix: "",
   },
-   content: {
+  content: {
     build: {
       markdown: {
         highlight: {
           theme: {
-            default: 'github-light',
-            dark: 'github-dark',
-            light: 'github-light',
-          }
-        }
-      }
-    }
-  }
-})
+            default: "github-light",
+            dark: "github-dark",
+            light: "github-light",
+          },
+        },
+      },
+    },
+  },
+});
